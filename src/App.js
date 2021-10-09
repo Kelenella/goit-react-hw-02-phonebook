35d61af49238a727a86809505ed8f376964952bd
Jane Yushchenko <kelenella@ukr.net>
2021-10-10 00:07:59 +0300

    create ContactForm

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
-export default function App() {
-  return <div></div>;
+import React from 'react';
+import ContactForm from './components/ContactForm/ContactForm';
+class App extends React.Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  render() {
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <h2>Contacts</h2>
+        <ul></ul>
+      </>
+    );
+  }
 }
+export default App;
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class ContactForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <label>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+          />
+        </label>
+        <button>Add contact</button>
+      </form>
+    );
+  }
+}
+
+export default ContactForm;
